40ad20b368997e2031b75097d9e35929a4fd0b1e
akt1mel <kostyavas.94@mail.ru>
2019-02-22 21:34:34 +0300

    выполнены задания

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const itemsList = document.querySelector('.items-list');
+
+function addItem(event) {
+    event.preventDefault();
+    const product = {
+        'title': event.target.dataset.title,
+        'price': event.target.dataset.price
+    };
+
+    if (event.target.classList.contains('add-to-cart')) {
+        addToCart(product);
+    }
+}
+
+itemsList.addEventListener('click', addItem);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+    if(!(event.target.classList.contains('prop__name'))){
+       return;
+    }
+  event.target.dataset.dir = event.target.dataset.dir === '1' ? '-1' : '1';
+
+  table.dataset.sortBy = event.target.dataset.propName;
+
+  sortTable(event.target.dataset.propName, event.target.dataset.dir);
+
 }
